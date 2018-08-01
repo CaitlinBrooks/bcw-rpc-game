d5ab07139989aeacb0a6c19886493a4cc2672933
CaitlinCodes <caiit@me.com>
2018-08-01 14:19:20 -0600

    formatting

--- app/app.js
+++ app/app.js
@@ -1,12 +1,26 @@
-function play (playersChoice) {
-    if(playersChoice === rock){
+function play(playersChoice) {
+    let computerChoice = "rock"
+    if (playersChoice === 'rock') {
 
-    } else if(x === paper){
-        
-    } else if(x === scissors){
+        if (computerChoice === 'rock') {
+            return "Tie Game"
+        }
+        if (computerChoice === 'paper') {
+            return "Computer wins"
+        }
+        return "User wins"
+    if (playersChoice === 'paper') {
     
-    } else{
+        if (computerChoice === 'rock') {
+            return "User wins"
+        }
+        if (computerChoice === 'paper') {
+            return "Tie Game"
+        }
+        return "Computer wins"
+    }
+    } else {
 
     }
-    
+
 }
