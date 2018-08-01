19a0313eb9d078658c5eb8d35e390ec0d4a7373e
CaitlinCodes <caiit@me.com>
2018-08-01 14:36:12 -0600

    last else statement

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 function play(playersChoice) {
-    let computerChoice = "rock"
+    let computerChoice = 'rock'
     if (playersChoice === 'rock') {
 
         if (computerChoice === 'rock') {
@@ -9,18 +9,15 @@ function play(playersChoice) {
             return "Computer wins"
         }
         return "User wins"
-    if (playersChoice === 'paper') {
-    
-        if (computerChoice === 'rock') {
-            return "User wins"
-        }
-        if (computerChoice === 'paper') {
-            return "Tie Game"
-        }
-        return "Computer wins"
-    }
-    } else {
+        if (playersChoice === 'paper') {
 
-    }
-
+            if (computerChoice === 'rock') {
+                return "User wins"
+            }
+            if (computerChoice === 'paper') {
+                return "Tie Game"
+            }
+            return "Computer wins"
+        }
+    } else
 }
